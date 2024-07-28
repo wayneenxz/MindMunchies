ae20bb2e450b94daf6515ad654d4c5f4a4bfbd5c
Ng Xu Zhe Wayne <e1115315@u.nus.edu>
2024-07-29 01:09:21 +0800

    changes

--- MindMunchies/venv/Scripts/activate.bat
+++ MindMunchies/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\nasru\Desktop\AIML Projects\wen xuan\MindMunchies\venv
+set VIRTUAL_ENV=C:\Users\wayne\MindMunchies\MindMunchies\venv
 
 if not defined PROMPT set PROMPT=$P$G
 

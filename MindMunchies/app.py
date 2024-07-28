5cf555db861766a94a67b09afdf21e75537af856
Ng Xu Zhe Wayne <e1115315@u.nus.edu>
2024-07-29 01:37:44 +0800

    change

---
+++ MindMunchies/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
+from app import create_app, db
+from app.models import User, Flashcard
+from app.forms import RegistrationForm, LoginForm
+
+app = create_app()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Flashcard': Flashcard}
+
+if __name__ == '__main__':
+    app.run(debug=True)
